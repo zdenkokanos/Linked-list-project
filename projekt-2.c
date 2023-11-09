30a01326dc93d3633ffefd9d31a899c30fad0ffc
Zdenko Kanoš <xkanos@stuba.sk>
2023-11-09 16:46:45 +0100

    pridaný základ funkcie n

--- projekt-2.c
+++ projekt-2.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+FILE f_n(FILE *file_data, bool *n_was_started)
+{
+    if (n_was_started == false)
+    {
+        file_data = fopen("dataloger_V2.txt", "r");
+        if (file_data == NULL)
+        {
+            printf("Neotvorený súbor.\n");
+        }
+    }
+    return *file_data;
+}
+
+void f_v(FILE *file_data)
+{
+    if (file_data != NULL)
+    {
+        printf("Bol otvoreny.\n");
+    }
+}
 
 int main()
 {
-    //toto som sem pridal
+    FILE *file_data;
+    char input;
+    bool *n_was_started = false;
+    while (1)
+    {
+        scanf("%c", &input);
+        switch (input)
+        {
+        case 'n':
+            f_n(file_data, n_was_started);
+            break;
+        case 'v':
+            f_v(file_data);
+            break;
+        default:
+            break;
+        }
+    }
+
     return 0;
 }
