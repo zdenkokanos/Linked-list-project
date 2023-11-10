9c7263ddd8ab1ecb71a9c67e7e8d9a18236cb903
Zdenko Kanoš <xkanos@stuba.sk>
2023-11-10 14:41:38 +0100

    pridanych par veci

--- projekt-2.c
+++ projekt-2.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-FILE f_n(FILE *file_data, bool *n_was_started)
+typedef struct id_mer_modulu
+{
+    char oznacenie;
+    int cislovanie;
+    char druh;
+} ID_MOD;
+
+typedef struct pos_mer_modulu
+{
+    double latitude;
+    double longitude;
+} POS_MOD;
+
+typedef struct mer_modul
+{
+    ID_MOD *id_mer_modulu;
+    POS_MOD *pos_mer_modulu;
+    char typ;
+    double hodnota;
+    int cas_num;
+    int date;
+} MODUL;
+
+FILE f_n(FILE *file_data, bool *n_was_started, int *data_count)
 {
     if (n_was_started == false)
     {
@@ -10,6 +34,22 @@ FILE f_n(FILE *file_data, bool *n_was_started)
         {
             printf("Neotvorený súbor.\n");
         }
+        char data;
+        for (data = fgetc(file_data); data != EOF; data = fgetc(file_data))
+        {
+            if (data == '$')
+            {
+                (*data_count)++;
+            }
+        }
+        (*data_count) /= 3;
+        fseek(file_data, 0, SEEK_SET);
+
+        ID_MOD *id_mer_modulu = malloc((*data_count) * sizeof(ID_MOD));
+        POS_MOD *pos_mer_modulu = malloc((*data_count) * sizeof(POS_MOD));
+        MODUL *mer_modul = malloc((*data_count) * sizeof(MODUL));
+
+        printf("%d", (*data_count));
     }
     return *file_data;
 }
@@ -26,6 +66,7 @@ int main()
 {
     FILE *file_data;
     char input;
+    int data_count = 0;
     bool *n_was_started = false;
     while (1)
     {
@@ -33,7 +74,7 @@ int main()
         switch (input)
         {
         case 'n':
-            f_n(file_data, n_was_started);
+            f_n(file_data, n_was_started, &data_count);
             break;
         case 'v':
             f_v(file_data);
